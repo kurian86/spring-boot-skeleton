d0613a8921b9ee0de7fdacd89653e07740bd4a3f
Ángel María Siles López <hi@asiles.dev>
2026-02-10 18:16:55 +0100

    enable caching

--- src/main/kotlin/es/bdo/skeleton/SkeletonApplication.kt
+++ src/main/kotlin/es/bdo/skeleton/SkeletonApplication.kt
@@ -3,9 +3,11 @@ package es.bdo.skeleton
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableCaching
 class SkeletonApplication
 
 fun main(args: Array<String>) {
