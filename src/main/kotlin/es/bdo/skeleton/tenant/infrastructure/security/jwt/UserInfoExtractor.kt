0ec40f89c14ccae87a719202cca41014dd95e9fd
Ángel María Siles López <hi@asiles.dev>
2026-02-12 17:49:29 +0100

    format

--- src/main/kotlin/es/bdo/skeleton/tenant/infrastructure/security/jwt/UserInfoExtractor.kt
+++ src/main/kotlin/es/bdo/skeleton/tenant/infrastructure/security/jwt/UserInfoExtractor.kt
@@ -4,6 +4,7 @@ import es.bdo.skeleton.tenant.infrastructure.security.UserInfo
 import org.springframework.security.oauth2.jwt.Jwt
 
 interface UserInfoExtractor {
+
     fun supports(issuer: String): Boolean
     
     fun extract(jwt: Jwt): UserInfo
